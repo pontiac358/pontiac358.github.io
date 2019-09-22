bf03d39a210a0f60a4cf17c927d672549e842d0a
mac <pontiac358@gmail.com>
2019-09-22 19:45:27 +0300

    fetch

---
+++ precache-manifest.2679ddd04bd0a7b289c3e857475a845e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7c326132d5afad0255c",
+    "url": "/static/js/main.0dde4e47.chunk.js"
+  },
+  {
+    "revision": "186e11401cf6994a2d13",
+    "url": "/static/js/2.c137d7c7.chunk.js"
+  },
+  {
+    "revision": "f7c326132d5afad0255c",
+    "url": "/static/css/main.b089bf24.chunk.css"
+  },
+  {
+    "revision": "ff5ffea272f0e073432fd73f4235e594",
+    "url": "/index.html"
+  }
+];
